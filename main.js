7a23c36976ddda95e4905c1fd3dff484dcfbadb7
Mahmudjon2011 <dildooraxajayeva@gmail.com>
2025-01-15 10:03:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const ism = "Mahmud";
+
+const mahmud = prompt("Enter your name ");
+if(mahmud == ""){
+    alert("Iltimos ismingizni kiriting")
+}
+else  if(mahmud.toLowerCase().includes("h")){
+    console.log("mahmudjon13");
+    
+}else{
+    console.log("mahmud1");
+    
+}
+
